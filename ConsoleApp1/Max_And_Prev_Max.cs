1ba759da596c452db063664f7f3351073f4e23ff
MaximkaStar <max.martsinkus@mail.ru>
2018-11-01 14:38:26 +0600

    first commit

---
+++ ConsoleApp1/Max_And_Prev_Max.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Max_And_Prev_Max
+    {
+        public int PREV_MAX { get; set; }
+        public int MAX { get; set; }
+
+        public Max_And_Prev_Max()
+        {
+            PREV_MAX = 0;
+            MAX = 0;
+        }
+        public void SetMax(int Number)
+        {
+            PREV_MAX = MAX;
+            MAX = Number;
+        }
+    }
+}
